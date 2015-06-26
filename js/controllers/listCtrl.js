274815e88a910f0fe53028904ac7e21e1e03371c
Alexander Kochergin <ejiqpep@gmail.com>
2015-06-26 08:22:10 +0300

    Added part3

--- js/controllers/listCtrl.js
+++ js/controllers/listCtrl.js
@@ -1,5 +1,14 @@
-angular.module('app').controller('listCtrl', function (listFactory) {
+angular.module('app').controller('listCtrl', function (listFactory, cardFactory) {
   this.removeList = function (list) {
     listFactory.removeList(list);
+  };
+
+  this.getCards = function (list) {
+    return cardFactory.getCards(list);
+  };
+
+  this.createCard = function (list) {
+    cardFactory.createCard(list, this.cardDescription);
+    this.cardDescription = '';
   };
 });
---
+++ js/factories/cardFactory.js
@@ -0,0 +1,35 @@
+angular.module('app').factory('cardFactory', function () {
+  var service = {};
+
+  var cards = [
+    {
+      id: 1,
+      description: 'Fix bug in player',
+      list_id: 1
+    },
+    {
+      id: 2,
+      description: 'Add feature with D3',
+      list_id: 1
+    },
+    {
+      id: 3,
+      description: 'Learn AngularJS',
+      list_id: 3
+    }
+  ];
+
+  service.getCards = function (list) {
+    return _.filter(cards, { list_id: list.id });
+  };
+
+  service.createCard = function (list, cardDescription) {
+    cards.push({
+      id: _.uniqueId('card_'),
+      description: cardDescription,
+      list_id: list.id
+    });
+  };
+
+  return service;
+});
