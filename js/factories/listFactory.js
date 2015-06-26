b4b3eb761d6eab42ed45a67e98a8388e84c85d35
Alexander Kochergin <ejiqpep@gmail.com>
2015-06-26 08:20:48 +0300

    Added part2

--- js/factories/listFactory.js
+++ js/factories/listFactory.js
@@ -20,5 +20,16 @@ angular.module('app').factory('listFactory', function () {
     return lists;
   };
 
+  service.addList = function (listName) {
+    lists.push({
+      id: _.uniqueId('list_'),
+      listName: listName
+    });
+  };
+
+  service.removeList = function (list) {
+    _.pull(lists, list);
+  };
+
   return service;
 });
